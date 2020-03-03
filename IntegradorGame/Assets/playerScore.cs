041826b5d48014ce3bedea9ae5dc8784a00db229
Andres Felipe Herrera Jerez <33936503+Andresxv@users.noreply.github.com>
2020-03-03 01:05:04 -0500

    moviles

---
+++ IntegradorGame/Assets/playerScore.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Proyecto26;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class playerScore : MonoBehaviour
+{
+    public Text scoreText;
+    public static int score;
+    public static string playerName;
+    private System.Random random= new System.Random();
+    public InputField nameText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = random.Next(1000);
+        scoreText.text="puntaje: "+score;
+    }
+    public void OnSumit()
+    {
+        playerName = nameText.text;
+        dataBase();
+    }
+    private void dataBase()
+    {
+        user user = new user();
+        RestClient.Put("https://usuarios-ff410.firebaseio.com/", user);
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ IntegradorGame/Assets/user.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[Serializable]
+public class user 
+{
+    public string userName;
+    public int userScore;
+    public user()
+    {
+        userName = playerScore.playerName;
+        userScore = playerScore.score;
+    }
+}
